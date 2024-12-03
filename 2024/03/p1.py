e3460ac2c790fb356cd8c6a62100338ea8150bec
Emmanuel Vendé <47533717+emmanuelvende@users.noreply.github.com>
2024-12-03 16:50:00 +0100

    03-1

--- 2024/03/p1.py
+++ 2024/03/p1.py
@@ -4,14 +4,11 @@ with open("in.txt", "r") as f:
     lines = f.read()
 
 
-def scan(s):
-    m = re.findall(r"mul\(\d+,\d+\)", s)
-    if m:
-        result = 0
-        for p in m:
-            n = re.findall("\d+", p)
-            result += int(n[0]) * int(n[1])
-    print(result)
+m = re.findall(r"mul\(\d+,\d+\)", lines)
+if m:
+    result = 0
+    for p in m:
+        n = re.findall("\d+", p)
+        result += int(n[0]) * int(n[1])
+print(result)
 
-
-scan(lines)
