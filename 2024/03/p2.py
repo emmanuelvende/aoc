7a29252357f0178d7325041fbf974a2e9434a5d0
Emmanuel Vendé <47533717+emmanuelvende@users.noreply.github.com>
2024-12-03 16:18:29 +0100

    03-2

--- 2024/03/p2.py
+++ 2024/03/p2.py
@@ -1,48 +1,27 @@
+import re
+
 with open("in.txt", "r") as f:
-    lines = f.read().split("\n")
-
-reports = list(list(map(int, line.split())) for line in lines)
+    lines = f.read()
 
 
-def safe_increase_condition(prev, next):
-    return next > prev and 1 <= abs(next - prev) <= 3
+def scan(s):
+    muls = list(m for m in re.finditer(r"mul\(\d+,\d+\)", s))
+    doess = list(m for m in re.finditer(r"do\(\)", s))
+    donts = list(m for m in re.finditer(r"don't\(\)", s))
+    tokens = muls + doess + donts
+    tokens.sort(key=lambda x: x.span()[0])
+    result = 0
+    enabled = True
+    for token in tokens:
+        word = token.group()
+        if word.startswith("mul") and enabled:
+            nn = re.findall("\d+", word)
+            result += int(nn[0]) * int(nn[1])
+        elif word.startswith("do()"):
+            enabled = True
+        elif word.startswith("don't()"):
+            enabled = False
+    print(result)
 
-
-def safe_decrease_condition(prev, next):
-    return next < prev and 1 <= abs(next - prev) <= 3
-
-
-def safe_progressing(report, condition):
-    safe = True
-    prev = report[0]
-    for next in report[1:]:
-        if condition(prev, next):
-            prev = next
-            continue
-        safe = False
-        break
-    return safe
-
-
-def meta_safe_progressing(report, condition):
-    n = 0
-    for i in range(len(report)):
-        new_report = report[:i] + report[i + 1 :]
-        if safe_progressing(new_report, condition):
-            n += 1
-    return n > 0
-
-
-def is_safe(report):
-    return (
-        safe_progressing(report, safe_increase_condition)
-        or safe_progressing(report, safe_decrease_condition)
-        or meta_safe_progressing(report, safe_increase_condition)
-        or meta_safe_progressing(report, safe_decrease_condition)
-    )
-
-
-safes = list(report for report in reports if is_safe(report))
-
-result = len(safes)
-print(result)
+# lines = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+scan(lines)
