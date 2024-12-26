70bccc6e9b5945316622fa8960dab36f83bab213
emmanuelvende <emmanuel@vende.fr>
2024-12-26 12:45:15 +0100

    2015-10-1

---
+++ 2015/10/p1.py
@@ -0,0 +1,32 @@
+import pyperclip
+import sys
+
+
+def pr(x):
+    print(x)
+    pyperclip.copy(x)
+
+
+s = open(sys.argv[1], "r").read()
+
+
+def compute(s):
+    ns = ""
+    p = s[0]
+    c = 1
+    for x in s[1:]:
+        if x == p:
+            c += 1
+        else:
+            ns += f"{c}{p}"
+            p = x
+            c = 1
+    ns += f"{c}{p}"
+    return ns
+
+
+for i in range(40):
+    s = compute(s)
+
+
+pr(len(s))
