93c633d70f5fe44a9ce6ae8a963189b8f4f0ea7a
emmanuelvende <emmanuel@vende.fr>
2024-12-28 14:23:12 +0100

    2015-12-2

--- 2015/12/p2.py
+++ 2015/12/p2.py
@@ -13,32 +13,25 @@ s = open(sys.argv[1], "r").read()
 
 j = json.loads(s)
 
-# clean the json from everythin that is red
+
 def clean_red(j):
     if type(j) == list:
-        return j
+        cleaned = []
+        for x in j:
+            cleaned.append(clean_red(x))
+        return cleaned
     elif type(j) == dict:
         if "red" in j.values():
             return {}
         cleaned = {}
         for k, v in j.items():
             cleaned[k] = clean_red(v)
+        return cleaned
     else:
         return j
 
 
-
-
-    #     if "red" in j:
-    #         return []
-    #     cleaned = []
-    #     for x in j:
-    #         if type(x) in (list, dict):
-    #             cleaned.append(clean_red(x))
-    #         else:
-    #             cleaned.append(x)
-    # elif type(j) == dict:
-    #     pass
+j = clean_red(j)
 
 s = json.dumps(j)
 m = re.findall("-?\d+\.?\d*", s)
