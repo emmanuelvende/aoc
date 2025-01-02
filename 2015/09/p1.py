7abee53b1c214f01fbf7dba58fd06e6d15f6e728
emmanuelvende <emmanuel@vende.fr>
2025-01-02 19:19:55 +0100

    2015-09-1

--- 2015/09/p1.py
+++ 2015/09/p1.py
@@ -2,6 +2,7 @@ import sys
 import pyperclip
 import itertools
 
+
 def pr(x):
     print(x)
     pyperclip.copy(x)
@@ -13,18 +14,27 @@ nodes = set()
 D = {}
 
 for line in L:
-    left, dist = line.split('=')
+    left, dist = line.split("=")
     dist = int(dist)
     src, _, dst = left.split()
     if src not in D:
         D[src] = {}
     D[src][dst] = dist
+    if dst not in D:
+        D[dst] = {}
+    D[dst][src] = dist
     nodes.add(src)
     nodes.add(dst)
 
-print(nodes)
-print(D)
-
 
 P = list(itertools.permutations(nodes))
-print(len(P))
+r = sys.maxsize
+for p in P:
+    r0 = 0
+    for i, n in enumerate(p[:-1]):
+        m = p[i + 1]
+        r0 += D[n][m]
+    if r0 < r:
+        r = r0
+
+pr(r)
